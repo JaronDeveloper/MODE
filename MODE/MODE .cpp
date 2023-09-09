96f7e29ce1d4fa44e400c671fe505671b21f0b4f
Prodject <Liter.Hari@gmail.com>
2023-09-09 14:33:25 +0200

    first commit

--- MODE/MODE .cpp
+++ MODE/MODE .cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 
-#define MODE 1
+#define MODE 
 
 int add(int a, int b) {
 	return a + b;
 }
 
-int main() {	
+int main() {
 	setlocale(LC_ALL, "Russian");
+	int a = 0;
 
- #if MODE == 0
-	std::cout << "Работаю в режиме тренировки" << std::endl;
-  #elif MODE == 1
-	std::cout << "Работаю в боевом режиме" << std::endl;
-	int a, b;
-	std::cout << "Введите число 1: ";
-	std::cin >> a;
-	std::cout << "Введите число 2: ";
-	std::cin >> b;
-	std::cout << "Результат сложения: " << add(a,b) <<std::endl;
-  #else
-	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
-  #error examination MODE
- #endif
+#if defined MODE
+	a = 1;
+#else
+	std::cout << "Константа не определена MODE" << std::endl;
+#endif
+	
+	if (a == 1) {
+#if MODE == 0
+		std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+		std::cout << "Работаю в боевом режиме" << std::endl;
+		int a, b;
+		std::cout << "Введите число 1: ";
+		std::cin >> a;
+		std::cout << "Введите число 2: ";
+		std::cin >> b;
+		std::cout << "Результат сложения: " << add(a, b) << std::endl;
+#else
+		std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+	}
 
 	return 0;
 }
